398b2771e9bca44f405c28aeb1dca628b75f8f11
Will Pragnell <wpragnell@pivotal.io>
2015-02-11 14:07:56 +0000

    fix timer leaks and extract timeout in resetter.go

--- resetter/resetter.go
+++ resetter/resetter.go
@@ -28,6 +28,7 @@ type Resetter struct {
 	portChecker     portChecker
 	shell           Shell
 	monitExecutable string
+	timeout         time.Duration
 }
 
 func New(defaultConfPath string, confPath string, portChecker portChecker, shell Shell, monitExecutable string) *Resetter {
@@ -37,11 +38,42 @@ func New(defaultConfPath string, confPath string, portChecker portChecker, shell
 		portChecker:     portChecker,
 		shell:           shell,
 		monitExecutable: monitExecutable,
+		timeout:         time.Second * 10,
 	}
 }
 
-func (Resetter *Resetter) DeleteAllData() error {
-	Resetter.shell.Run(exec.Command(Resetter.monitExecutable, "stop", "redis"))
+func (resetter *Resetter) DeleteAllData() error {
+	if err := resetter.stopRedis(); err != nil {
+		return err
+	}
+
+	if err := resetter.deleteData(); err != nil {
+		return err
+	}
+
+	if err := resetter.resetConf(); err != nil {
+		return err
+	}
+
+	if err := resetter.startRedis(); err != nil {
+		return err
+	}
+
+	credentials, err := credentials.Parse(resetter.confPath)
+	if err != nil {
+		return err
+	}
+
+	address, err := net.ResolveTCPAddr("tcp", fmt.Sprintf("127.0.0.1:%d", credentials.Port))
+	if err != nil {
+		return err
+	}
+
+	return resetter.portChecker.Check(address, resetter.timeout)
+}
+
+func (resetter *Resetter) stopRedis() error {
+	resetter.shell.Run(exec.Command(resetter.monitExecutable, "stop", "redis"))
 	redisProcessDead := make(chan bool)
 	go func(c chan<- bool) {
 		for {
@@ -55,25 +87,23 @@ func (Resetter *Resetter) DeleteAllData() error {
 		}
 	}(redisProcessDead)
 
+	timer := time.NewTimer(resetter.timeout)
+	defer timer.Stop()
 	select {
 	case <-redisProcessDead:
-	case <-time.After(time.Second * 10):
+		break
+	case <-timer.C:
 		return errors.New("timed out waiting for redis process to die after 10 seconds")
 	}
 
-	err := os.Remove("appendonly.aof")
-	if err != nil {
-		return err
-	}
-	os.Remove("dump.rdb")
-	err = Resetter.resetConf()
-	if err != nil {
-		return err
-	}
+	return nil
+}
+
+func (resetter *Resetter) startRedis() error {
 	redisStarted := make(chan bool)
 	go func(c chan<- bool) {
 		for {
-			_, err := Resetter.shell.Run(exec.Command(Resetter.monitExecutable, "start", "redis"))
+			_, err := resetter.shell.Run(exec.Command(resetter.monitExecutable, "start", "redis"))
 			if err == nil {
 				c <- true
 				return
@@ -81,31 +111,37 @@ func (Resetter *Resetter) DeleteAllData() error {
 			time.Sleep(time.Millisecond * 100)
 		}
 	}(redisStarted)
+
+	timer := time.NewTimer(resetter.timeout)
+	defer timer.Stop()
 	select {
 	case <-redisStarted:
-	case <-time.After(time.Second * 10):
+		break
+	case <-timer.C:
 		return errors.New("timed out waiting for redis process to be started by monit after 10 seconds")
 	}
-	credentials, err := credentials.Parse(Resetter.confPath)
-	if err != nil {
-		return err
-	}
-	address, err := net.ResolveTCPAddr("tcp", fmt.Sprintf("127.0.0.1:%d", credentials.Port))
-	if err != nil {
-		return err
-	}
-	return Resetter.portChecker.Check(address, time.Second*10)
+
+	return nil
 }
 
-func (Resetter *Resetter) resetConf() error {
-	conf, err := redisconf.Load(Resetter.defaultConfPath)
+func (_ *Resetter) deleteData() error {
+	if err := os.Remove("appendonly.aof"); err != nil {
+		return err
+	}
+
+	os.Remove("dump.rdb")
+	return nil
+}
+
+func (resetter *Resetter) resetConf() error {
+	conf, err := redisconf.Load(resetter.defaultConfPath)
 	if err != nil {
 		return err
 	}
 
 	conf.Set("requirepass", uuid.NewRandom().String())
-	err = conf.Save(Resetter.confPath)
-	if err != nil {
+
+	if err := conf.Save(resetter.confPath); err != nil {
 		return err
 	}
 
