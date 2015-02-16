fe03d9ffe07149c48d034b8d6173c469f31f2722
Svett Ralchev <sralchev@pivotal.io>
2015-02-16 16:49:32 +0000

    use latest stable docker image on ci
    Signed-off-by: Will Pragnell <wpragnell@pivotal.io>

--- script/ci.sh
+++ script/ci.sh
@@ -10,4 +10,4 @@ docker run \
   -e "GOPATH=/home/user/go" \
   -v $PWD:/home/user/go/src/github.com/pivotal-cf/cf-redis-broker \
   -w /home/user/go/src/github.com/pivotal-cf/cf-redis-broker\
-  cflondonservices/london-services-ci-redis:976a703cfe079cb60047a3b3ab8d57cfc5601b06 ./script/test
+  cflondonservices/london-services-ci-redis:stable ./script/test
