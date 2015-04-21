5c4e1ef100e94d7371dbdc051c2aae4f66554cf1
Stev Witzel <switzel@pivotal.io>
2015-04-21 17:40:11 +0100

    Ability to run specific tests locally, to aid debugging
    Signed-off-by: Jamie Monserrate <jmonserrate@pivotal.io>

--- script/ci.sh
+++ script/ci.sh
@@ -10,4 +10,4 @@ docker run \
   -e "GOPATH=/home/user/go" \
   -v $PWD:/home/user/go/src/github.com/pivotal-cf/cf-redis-broker \
   -w /home/user/go/src/github.com/pivotal-cf/cf-redis-broker\
-  cflondonservices/london-services-ci-redis:stable ./script/test
+  cflondonservices/london-services-ci-redis:stable ./script/test $@
