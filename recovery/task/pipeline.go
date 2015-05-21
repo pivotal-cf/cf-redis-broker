1992412fa0ab36e378aa8286c53bf5b22da80b07
Jamie Monserrate <jmonserrate@pivotal.io>
2015-05-21 11:31:49 +0100

    Tests for Pipeline
    Signed-off-by: Stev Witzel <switzel@pivotal.io>

--- recovery/task/pipeline.go
+++ recovery/task/pipeline.go
@@ -2,7 +2,7 @@ package task
 
 import "github.com/pivotal-golang/lager"
 
-type Pipeline struct {
+type pipeline struct {
 	name   string
 	logger lager.Logger
 	tasks  []Task
@@ -13,44 +13,50 @@ func NewPipeline(
 	logger lager.Logger,
 	tasks ...Task,
 ) Task {
-	return &Pipeline{
+	return &pipeline{
 		logger: logger,
 		tasks:  tasks,
 		name:   name,
 	}
 }
 
-func (p *Pipeline) Name() string {
+func (p *pipeline) Name() string {
 	return p.name
 }
 
-func (p *Pipeline) Run(artifact Artifact) (Artifact, error) {
+func (p *pipeline) Run(artifact Artifact) (Artifact, error) {
 	var err error
 	for _, task := range p.tasks {
-		p.logger.Info("pipleline-task",
-			lager.Data{
-				"event":    "starting",
-				"pipeline": p.Name(),
-				"task":     task.Name(),
-			},
-		)
+		p.logInfo("starting", task)
 
 		artifact, err = task.Run(artifact)
 		if err != nil {
-			p.logger.Error("pipleline-step",
-				err,
-				lager.Data{
-					"event": "failed",
-				},
-			)
+			p.logError(err, task)
 			return nil, err
 		}
 
-		p.logger.Info("pipleline-step",
-			lager.Data{
-				"event": "done",
-			},
-		)
+		p.logInfo("done", task)
 	}
 	return artifact, err
+}
+
+func (p *pipeline) logInfo(event string, task Task) {
+	p.logger.Info("pipleline-step",
+		lager.Data{
+			"event":    event,
+			"pipeline": p.Name(),
+			"task":     task.Name(),
+		},
+	)
+}
+
+func (p *pipeline) logError(err error, task Task) {
+	p.logger.Error("pipleline-step",
+		err,
+		lager.Data{
+			"event":    "failed",
+			"pipeline": p.Name(),
+			"task":     task.Name(),
+		},
+	)
 }
---
+++ recovery/task/pipeline_test.go
@@ -0,0 +1,122 @@
+package task_test
+
+import (
+	"errors"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gbytes"
+	"github.com/pivotal-cf/cf-redis-broker/recovery/task"
+	"github.com/pivotal-golang/lager"
+)
+
+type fakeTask struct {
+	ExpectedErr error
+	Artifact    task.Artifact
+	TaskName    string
+}
+
+func (f *fakeTask) Name() string {
+	return f.TaskName
+}
+
+func (f *fakeTask) Run(artifact task.Artifact) (task.Artifact, error) {
+	f.Artifact = artifact
+	return task.NewArtifact(f.Name()), f.ExpectedErr
+}
+
+var _ = Describe("Pipeline", func() {
+	var (
+		logger lager.Logger
+		log    *gbytes.Buffer
+	)
+
+	BeforeEach(func() {
+		logger = lager.NewLogger("logger")
+		log = gbytes.NewBuffer()
+		logger.RegisterSink(lager.NewWriterSink(log, lager.INFO))
+	})
+
+	Describe(".Name", func() {
+		It("returns the pipeline name", func() {
+			pipeline := task.NewPipeline("some-pipeline", logger)
+
+			Expect(pipeline.Name()).To(Equal("some-pipeline"))
+		})
+
+	})
+
+	Describe(".Run", func() {
+		var (
+			task1            *fakeTask
+			task2            *fakeTask
+			originalArtifact = task.NewArtifact("path/to/artifact")
+			finalArtifact    task.Artifact
+			runErr           error
+		)
+
+		Context("with multiple successful tasks", func() {
+			BeforeEach(func() {
+				task1 = &fakeTask{TaskName: "task1"}
+				task2 = &fakeTask{TaskName: "task2"}
+
+				pipeline := task.NewPipeline("some-name", logger, task1, task2)
+
+				finalArtifact, runErr = pipeline.Run(originalArtifact)
+			})
+
+			It("executes all tasks with given artifact", func() {
+				Expect(task1.Artifact).To(Equal(originalArtifact))
+				Expect(task2.Artifact.Path()).To(Equal("task1"))
+			})
+
+			It("returns the artifact of the last task", func() {
+				Expect(finalArtifact.Path()).To(Equal("task2"))
+			})
+
+			It("logs each step", func() {
+				Expect(log).To(gbytes.Say(`"event":"starting","pipeline":"some-name","task":"task1"}`))
+				Expect(log).To(gbytes.Say(`"event":"done","pipeline":"some-name","task":"task1"}`))
+				Expect(log).To(gbytes.Say(`"event":"starting","pipeline":"some-name","task":"task2"}`))
+				Expect(log).To(gbytes.Say(`"event":"done","pipeline":"some-name","task":"task2"}`))
+			})
+
+			It("does not return an error", func() {
+				Expect(runErr).ToNot(HaveOccurred())
+			})
+		})
+
+		Context("when one of the tasks fails", func() {
+			var (
+				task3         *fakeTask
+				expectedError = errors.New("some-task-error")
+			)
+
+			BeforeEach(func() {
+				task1 = &fakeTask{TaskName: "task1"}
+				task2 = &fakeTask{TaskName: "task2", ExpectedErr: expectedError}
+				task3 = &fakeTask{TaskName: "task3"}
+
+				pipeline := task.NewPipeline("some-name", logger, task1, task2, task3)
+
+				finalArtifact, runErr = pipeline.Run(originalArtifact)
+			})
+
+			It("short circuits the tasks chain", func() {
+				Expect(task1.Artifact).ToNot(BeNil())
+				Expect(task2.Artifact).ToNot(BeNil())
+				Expect(task3.Artifact).To(BeNil())
+			})
+
+			It("returns the error", func() {
+				Expect(runErr).To(Equal(expectedError))
+			})
+
+			It("logs the error", func() {
+				Expect(log).To(gbytes.Say(
+					`"error":"some-task-error","event":"failed","pipeline":"some-name","task":"task2"}`,
+				))
+			})
+		})
+	})
+})
