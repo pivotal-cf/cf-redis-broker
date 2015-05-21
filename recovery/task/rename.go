5fdb4e9f4153d45c9e2e851f9da551ea4753d6fc
Jamie Monserrate <jmonserrate@pivotal.io>
2015-05-21 12:29:50 +0100

    Rename task implemented
    Signed-off-by: Stev Witzel <switzel@pivotal.io>

--- recovery/task/rename.go
+++ recovery/task/rename.go
@@ -1,22 +1,59 @@
 package task
 
-import "fmt"
+import (
+	"os"
+
+	"github.com/pivotal-golang/lager"
+)
 
 type rename struct {
 	target string
+	logger lager.Logger
 }
 
-func NewRename(target string) Task {
+func NewRename(target string, logger lager.Logger) Task {
 	return &rename{
 		target: target,
+		logger: logger,
 	}
 }
 
 func (r *rename) Run(a Artifact) (Artifact, error) {
-	fmt.Printf("Move %s to %s\n", a.Path(), r.target)
+	r.logInfo("starting", a.Path())
+
+	if err := os.Rename(a.Path(), r.target); err != nil {
+		r.logError(err, a.Path())
+		return nil, err
+	}
+
+	r.logInfo("done", a.Path())
+
 	return NewArtifact(r.target), nil
 }
 
 func (r *rename) Name() string {
 	return "rename"
+}
+
+func (r *rename) logInfo(event string, source string) {
+	r.logger.Info(r.Name(),
+		lager.Data{
+			"event":  event,
+			"source": source,
+			"target": r.target,
+			"task":   r.Name(),
+		},
+	)
+}
+
+func (r *rename) logError(err error, source string) {
+	r.logger.Error(r.Name(),
+		err,
+		lager.Data{
+			"event":  "failed",
+			"source": source,
+			"target": r.target,
+			"task":   r.Name(),
+		},
+	)
 }
---
+++ recovery/task/rename_test.go
@@ -0,0 +1,94 @@
+package task_test
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gbytes"
+	"github.com/pivotal-cf/cf-redis-broker/recovery/task"
+	"github.com/pivotal-golang/lager"
+)
+
+var _ = Describe("Rename", func() {
+	Describe(".Name", func() {
+		It("returns name", func() {
+			Expect(task.NewRename("blah", nil).Name()).To(Equal("rename"))
+		})
+	})
+
+	Describe(".Run", func() {
+		var (
+			tmpDirPath      string
+			originalPath    string
+			finalPath       string
+			renamedArtifact task.Artifact
+			runErr          error
+			logger          lager.Logger
+			log             *gbytes.Buffer
+		)
+
+		BeforeEach(func() {
+			tmpDirPath, err := ioutil.TempDir("", "temp")
+			Expect(err).ToNot(HaveOccurred())
+
+			finalPath = filepath.Join(tmpDirPath, "newName")
+			Expect(finalPath).ToNot(BeAnExistingFile())
+
+			file, err := ioutil.TempFile(tmpDirPath, "artifact")
+			Expect(err).ToNot(HaveOccurred())
+
+			originalPath = file.Name()
+			artifact := task.NewArtifact(originalPath)
+
+			logger = lager.NewLogger("logger")
+			log = gbytes.NewBuffer()
+			logger.RegisterSink(lager.NewWriterSink(log, lager.INFO))
+
+			renameTask := task.NewRename(finalPath, logger)
+			renamedArtifact, runErr = renameTask.Run(artifact)
+		})
+
+		AfterEach(func() {
+			os.Remove(tmpDirPath)
+		})
+
+		It("renames the artifact on disk", func() {
+			Expect(originalPath).ToNot(BeAnExistingFile())
+			Expect(finalPath).To(BeAnExistingFile())
+		})
+
+		It("returns artifact with the new path", func() {
+			Expect(renamedArtifact.Path()).To(Equal(finalPath))
+		})
+
+		It("does not return error", func() {
+			Expect(runErr).ToNot(HaveOccurred())
+		})
+
+		It("provides logging", func() {
+			Expect(log).To(gbytes.Say(fmt.Sprintf(`"event":"starting","source":"%s","target":"%s","task":"rename"}`, originalPath, finalPath)))
+			Expect(log).To(gbytes.Say(fmt.Sprintf(`"event":"done","source":"%s","target":"%s","task":"rename"}`, originalPath, finalPath)))
+		})
+
+		Context("when an error occurs", func() {
+			BeforeEach(func() {
+				originalPath = "path/to/nowhere"
+				artifact := task.NewArtifact(originalPath)
+				renameTask := task.NewRename(finalPath, logger)
+				renamedArtifact, runErr = renameTask.Run(artifact)
+			})
+
+			It("returns the error", func() {
+				Expect(runErr).To(BeAssignableToTypeOf(&os.LinkError{}))
+			})
+
+			It("logs the error", func() {
+				Expect(log).To(gbytes.Say(fmt.Sprintf(`"error":"%s","event":"failed","source":"%s","target":"%s","task":"rename"}`, runErr.Error(), originalPath, finalPath)))
+			})
+		})
+	})
+})
