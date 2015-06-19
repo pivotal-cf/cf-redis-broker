cc11a89fc39bba3395c832737b5e0af6380b52a2
Andrew Thorburn <athorburn@pivotal.io>
2015-06-19 10:43:06 +0100

    Pass env vars into docker
    Signed-off-by: Daniel Weinberg <danielw@cloudcredo.com>

--- script/ci.sh
+++ script/ci.sh
@@ -8,6 +8,8 @@ docker logout
 
 docker run \
   -e "GOPATH=/home/user/go" \
+  -e "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}" \
+  -e "AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}" \
   -v $PWD:/home/user/go/src/github.com/pivotal-cf/cf-redis-broker \
   -w /home/user/go/src/github.com/pivotal-cf/cf-redis-broker\
   cflondonservices/london-services-ci-redis:stable ./script/test $@
