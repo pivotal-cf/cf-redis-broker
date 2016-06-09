732ab54bfcdec068e6d73b8f65b804ccaaeb5614
Henry Stanley <hstanley@pivotal.io>
2016-06-09 11:04:13 +0100

    Run tests as vcap
    [#121118391]
    
    Signed-off-by: Jonathan Qiwen Tsang <jtsang@pivotal.io>

--- ci/test-redis-broker.sh
+++ ci/test-redis-broker.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-export GOPATH=$GOPATH:$PWD
+export PATH=$GOPATH/bin:$PATH
+export GOPATH=$PWD:$GOPATH
+
 cd src/github.com/pivotal-cf/cf-redis-broker
-./script/test
+
+# Run tests as vcap user
+useradd vcap
+sudo chown -R vcap:vcap /tmp
+
+sudo -E -u vcap ./script/test
