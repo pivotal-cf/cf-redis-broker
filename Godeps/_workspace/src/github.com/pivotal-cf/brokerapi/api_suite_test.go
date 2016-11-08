4878a3801778f5c11b466430e6d0599398ce1d0e
Jonathan Tsang <jtsang@pivotal.io>
2016-11-08 11:52:39 +0000

    Revert "Update go to 1.7.3"
    This reverts commit 9d08cb19fb6118e123765b9a0b09a08584d57f09.

---
+++ Godeps/_workspace/src/github.com/pivotal-cf/brokerapi/api_suite_test.go
@@ -0,0 +1,40 @@
+package brokerapi_test
+
+import (
+	"fmt"
+	"io/ioutil"
+	"path"
+	"testing"
+
+	"code.google.com/p/go-uuid/uuid"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestAPI(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "API Suite")
+}
+
+func fixture(name string) string {
+	filePath := path.Join("fixtures", name)
+	contents, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		panic(fmt.Sprintf("Could not read fixture: %s", name))
+	}
+
+	return string(contents)
+}
+
+func uniqueID() string {
+	return uuid.NewRandom().String()
+}
+
+func uniqueInstanceID() string {
+	return uniqueID()
+}
+
+func uniqueBindingID() string {
+	return uniqueID()
+}
