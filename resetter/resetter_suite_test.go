095636798299763e32c0ed3ac22b2e0c5c5fa284
BooleanCat <thgodkin@gmail.com>
2017-01-07 23:03:03 +0000

    remove junit reporter

--- resetter/resetter_suite_test.go
+++ resetter/resetter_suite_test.go
@@ -4,12 +4,10 @@ import (
 	"testing"
 
 	. "github.com/onsi/ginkgo"
-	"github.com/onsi/ginkgo/reporters"
 	. "github.com/onsi/gomega"
 )
 
 func TestResetter(t *testing.T) {
 	RegisterFailHandler(Fail)
-	junitReporter := reporters.NewJUnitReporter("junit_resetter.xml")
-	RunSpecsWithDefaultAndCustomReporters(t, "Resetter Suite", []Reporter{junitReporter})
+	RunSpecs(t, "Resetter Suite")
 }
