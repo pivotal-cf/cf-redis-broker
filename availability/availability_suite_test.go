5476434cef535737e5ef55f3ed0c58863d3c8fed
BooleanCat <thgodkin@gmail.com>
2017-01-08 12:44:32 +0000

    simplify availability specs

--- availability/availability_suite_test.go
+++ availability/availability_suite_test.go
@@ -1,6 +1,7 @@
 package availability_test
 
 import (
+	"net"
 	"testing"
 
 	. "github.com/onsi/ginkgo"
@@ -10,4 +11,16 @@ import (
 func TestAvailability(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Availability Suite")
+}
+
+func resolveTCPAddr(network, addr string) *net.TCPAddr {
+	address, err := net.ResolveTCPAddr(network, addr)
+	Expect(err).NotTo(HaveOccurred())
+	return address
+}
+
+func listenTCP(network string, laddr *net.TCPAddr) *net.TCPListener {
+	listener, err := net.ListenTCP(network, laddr)
+	Expect(err).NotTo(HaveOccurred())
+	return listener
 }
