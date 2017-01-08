4d8dd9ba5ea82213fb31801e8fc5d80eeeae8981
BooleanCat <thgodkin@gmail.com>
2017-01-08 12:30:00 +0000

    remove junit reporter from availability

--- availability/availability_suite_test.go
+++ availability/availability_suite_test.go
@@ -1,15 +1,13 @@
 package availability_test
 
 import (
-	. "github.com/onsi/ginkgo"
-	"github.com/onsi/ginkgo/reporters"
-	. "github.com/onsi/gomega"
-
 	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 )
 
 func TestAvailability(t *testing.T) {
 	RegisterFailHandler(Fail)
-	junitReporter := reporters.NewJUnitReporter("junit_availability.xml")
-	RunSpecsWithDefaultAndCustomReporters(t, "Availability Suite", []Reporter{junitReporter})
+	RunSpecs(t, "Availability Suite")
 }
