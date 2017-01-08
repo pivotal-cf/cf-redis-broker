44f6646c0402bce79af16e9b8c33222c3cfd8261
BooleanCat <thgodkin@gmail.com>
2017-01-08 00:48:29 +0000

    Revert "significantly simplify availability"
    This reverts commit b46b1c16c0a6e820512ae4eec79bec6bcbd2e382.

--- availability/ports.go
+++ availability/ports.go
@@ -1,28 +1,31 @@
 package availability
 
 import (
-	"errors"
 	"net"
 	"time"
 )
 
 func Check(address *net.TCPAddr, timeout time.Duration) error {
-	interval := time.Millisecond * 10
-	for elapsed := time.Duration(0); elapsed < timeout; elapsed = elapsed + interval {
-		if isListening(address) {
-			return nil
+	action := func(successChan chan<- struct{}, terminate <-chan struct{}) {
+		for {
+			select {
+			case <-terminate:
+				return
+			case <-time.After(10 * time.Millisecond):
+				if isListening(address) {
+					close(successChan)
+					return
+				}
+			}
 		}
-
-		time.Sleep(interval)
 	}
-
-	return errors.New("timeout")
+	enforcer := DeadlineEnforcer{
+		Action: action,
+	}
+	return enforcer.DoWithin(timeout)
 }
 
 func isListening(address *net.TCPAddr) bool {
-	connection, err := net.DialTCP("tcp", nil, address)
-	if connection != nil {
-		connection.Close()
-	}
+	_, err := net.DialTCP("tcp", nil, address)
 	return err == nil
 }
