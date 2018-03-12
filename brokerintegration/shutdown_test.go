8aff0fa7d6af112610df653f7c4d1552c38f03d6
Ed Cook <ecook@pivotal.io>
2018-03-12 16:23:43 +0000

    Switch assertions in shutdown_test to use Eventually
    Signed-off-by: Jack Newberry <jnewberry@pivotal.io>

--- brokerintegration/shutdown_test.go
+++ brokerintegration/shutdown_test.go
@@ -15,27 +15,24 @@ var _ = Describe("Broker Integration", func() {
 	BeforeEach(func() {
 		broker = integration.LaunchProcessWithBrokerConfig(brokerExecutablePath, "broker.yml-colocated")
 		Eventually(broker.Out).Should(gbytes.Say("shared Redis instance"))
+		helpers.KillProcess(broker)
 	})
 
 	AfterEach(func() {
 		helpers.KillProcess(broker)
 	})
 
-	JustBeforeEach(func() {
-		helpers.KillProcess(broker)
-	})
-
 	Context("when the Redis broker is shutdown", func() {
 		It("logs a shutdown preparation message", func() {
-			Expect(broker.Out).To(gbytes.Say("Starting Redis Broker shutdown"))
+			Eventually(broker.Out).Should(gbytes.Say("Starting Redis Broker shutdown"))
 		})
 
 		It("logs that it has identified zero shared instances", func() {
-			Expect(broker.Out).To(gbytes.Say("0 shared Redis instances found"))
+			Eventually(broker.Out).Should(gbytes.Say("0 shared Redis instances found"))
 		})
 
 		It("logs that it has identified zero dedicated instances", func() {
-			Expect(broker.Out).To(gbytes.Say("0 dedicated Redis instances found"))
+			Eventually(broker.Out).Should(gbytes.Say("0 dedicated Redis instances found"))
 		})
 	})
 })
