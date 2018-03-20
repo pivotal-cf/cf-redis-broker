36393f71cf0a581e424c179fd04491c3a99926b3
Ed Cook <ecook@pivotal.io>
2018-03-20 12:27:27 +0000

    Replace handwritten fake client with counterfeiter generated one

--- redis/client/client.go
+++ redis/client/client.go
@@ -13,6 +13,7 @@ import (
 	"github.com/pivotal-cf/cf-redis-broker/redisconf"
 )
 
+//go:generate counterfeiter -o fakes/fake_client.go . Client
 type Client interface {
 	Disconnect() error
 	WaitUntilRedisNotLoading(timeoutMilliseconds int) error
